4444a98feaaa27e9b022c834de5ad310ff4cbfe9
Eldar Akavov <desert73eagle@gmail.com>
2019-02-10 16:40:04 +0300

    update contacts and fixed GreetingController

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -13,15 +13,19 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
+
     @GetMapping("/about")
-    public String about(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String about(@RequestParam(name = "about", required = false, defaultValue = "batman") String about, Model model) {
+        model.addAttribute("about", about);
         return "about";
     }
+
     @GetMapping("/contacts")
-    public String contacts(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String contacts(@RequestParam(name = "mail", required = false, defaultValue = "soviet@mail.su") String mail,
+                           @RequestParam(name = "number", required = false, defaultValue = "+79245687978") String number,
+                           Model model) {
+        model.addAttribute("mail", mail);
+        model.addAttribute("number", number);
         return "contacts";
     }
-
 }
